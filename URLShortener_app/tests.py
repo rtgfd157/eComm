6f0a1dce5a287aa14539dc7e1950b8c01eb30e51
unknown <idanz1234u@example.com>
2020-06-24 21:51:57 +0300

    remove duplicate test and tests files

--- URLShortener_app/tests.py
+++ URLShortener_app/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import URLShortener
+from .forms import URLShortenerForm,URLShortenerUpdatingForm,test
 
-# Create your tests here.
+class URLShortenerTest(TestCase):
+    """
+    class for testing model view and form of URLShortener_app
+    """
+
+    url_mozl ='https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing'
+
+    urlshortener_too_long= URLShortener.objects.create(url=url_mozl,hash='ghh23hkjadafsdfasasdas')
+    urlshortener_good= URLShortener.objects.create(url=url_mozl,hash='ghh23')                                           
+    urlshortener_empty_hash= URLShortener.objects.create(url=url_mozl,hash='')
+    
+    # test form - too long hash for URLShortenerForm , URLShortenerUpdatingForm
+    def test_form_too_long_hash(self):
+        
+        #URLShortenerForm
+        data = {'url':self.urlshortener_too_long.url, 'hash': self.urlshortener_too_long.hash}
+        form = URLShortenerForm(data=data)
+        self.assertFalse(form.is_valid())
+
+        #URLShortenerUpdatingForm
+        data = {'url':self.urlshortener_too_long.url, 'hash': self.urlshortener_too_long.hash}
+        form = URLShortenerUpdatingForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
+    # test form - good inputs for URLShortenerForm , URLShortenerUpdatingForm
+    def test_form_good_valus(self):
+        
+        #URLShortenerForm
+        data = {'url':self.urlshortener_good.url, 'hash': self.urlshortener_good.hash}
+        form = URLShortenerForm(data=data)
+        self.assertTrue(form.is_valid())
+
+        #URLShortenerUpdatingForm
+        data = {'url':self.urlshortener_good.url, 'hash': self.urlshortener_good.hash}
+        form = URLShortenerUpdatingForm(data=data)
+        self.assertTrue(form.is_valid())
+
+
+    # check empty hash on model save
+    def test_model_empty_hash_method(self):
+        
+        # check empty hash
+        self.assertFalse(self.urlshortener_empty_hash.hash)
+
+        # check that make_hash model function add hash
+        self.urlshortener_empty_hash.hash = URLShortener.make_hash()
+        self.assertTrue(self.urlshortener_empty_hash.hash)
