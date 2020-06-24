b315173c5792193db2859e09f449acf5ebf2e248
unknown <idanz1234u@example.com>
2020-06-24 15:33:42 +0300

    dding check for regex for hash field

--- URLShortener_app/forms.py
+++ URLShortener_app/forms.py
@@ -1,12 +1,26 @@
 from django import forms
 from .models import URLShortener
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
+
+
+def test(val):
+    non_url_safe = ['"', '#', '$', '%', '&', '+',
+                    ',', '/', ':', ';', '=', '?',
+                    '@', '[', '\\', ']', '^', '`',
+                    '{', '|', '}', '~', "'"]
+
+    non_safe = [c for c in val if c in non_url_safe]
+    if non_safe:
+        raise ValidationError("Hash shouldn't be using : {}".format(non_url_safe))
 
 
 class URLShortenerForm(forms.ModelForm):
     """
         form for adding url and hash
     """
+
+    hash = forms.CharField( max_length = 15  ,required=False ,validators=[test]  )
 
     class Meta:
         model = URLShortener
@@ -17,6 +31,8 @@ class URLShortenerUpdatingForm(forms.ModelForm):
     """
     class for updating model with exsisting values for updating
     """
+    hash = forms.CharField( max_length = 15  ,required=False ,validators=[test]  )
+
     class Meta:
         model = URLShortener
         fields = ('url','hash')
