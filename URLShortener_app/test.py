71d3747d9d7cdf2fd236a4e57953990c48752df0
unknown <idanz1234u@example.com>
2020-06-24 21:45:24 +0300

    adding cosmetic changes ..

--- URLShortener_app/test.py
+++ URLShortener_app/test.py
@@ -3,14 +3,15 @@ from .models import URLShortener
 from .forms import URLShortenerForm,URLShortenerUpdatingForm,test
 
 class URLShortenerTest(TestCase):
+    """
+    class for testing model view and form of URLShortener_app
+    """
 
-    urlshortener_too_long= URLShortener.objects.create(url='https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing',
-                                                     hash='ghh23hkjadafsdfasasdas')
-    urlshortener_good= URLShortener.objects.create(url='https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing',
-                                                     hash='ghh23')
-                                            
-    urlshortener_empty_hash= URLShortener.objects.create(url='https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing',
-                                                     hash='')
+    url_mozl ='https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing'
+
+    urlshortener_too_long= URLShortener.objects.create(url=url_mozl,hash='ghh23hkjadafsdfasasdas')
+    urlshortener_good= URLShortener.objects.create(url=url_mozl,hash='ghh23')                                           
+    urlshortener_empty_hash= URLShortener.objects.create(url=url_mozl,hash='')
     
     # test form - too long hash for URLShortenerForm , URLShortenerUpdatingForm
     def test_form_too_long_hash(self):
@@ -19,7 +20,6 @@ class URLShortenerTest(TestCase):
         data = {'url':self.urlshortener_too_long.url, 'hash': self.urlshortener_too_long.hash}
         form = URLShortenerForm(data=data)
         self.assertFalse(form.is_valid())
-
 
         #URLShortenerUpdatingForm
         data = {'url':self.urlshortener_too_long.url, 'hash': self.urlshortener_too_long.hash}
@@ -35,7 +35,6 @@ class URLShortenerTest(TestCase):
         form = URLShortenerForm(data=data)
         self.assertTrue(form.is_valid())
 
-
         #URLShortenerUpdatingForm
         data = {'url':self.urlshortener_good.url, 'hash': self.urlshortener_good.hash}
         form = URLShortenerUpdatingForm(data=data)
@@ -47,7 +46,6 @@ class URLShortenerTest(TestCase):
         
         # check empty hash
         self.assertFalse(self.urlshortener_empty_hash.hash)
-
 
         # check that make_hash model function add hash
         self.urlshortener_empty_hash.hash = URLShortener.make_hash()
